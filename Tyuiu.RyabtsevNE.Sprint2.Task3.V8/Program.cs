0769e299dcf96ff7b4bc7ad8ceca6505e7c1270b
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-10-15 19:52:51 +0500

    Task3

---
+++ Tyuiu.RyabtsevNE.Sprint2.Task3.V8/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.RyabtsevNE.Sprint2.Task3.V8.Lib;
+namespace Tyuiu.RyabtsevNE.Sprint2.Task3.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Рябцев Н. Е. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнила: Рябцев Никита Евгеньевич | СМАРТб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить до 3х знаков после запятой*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите значение переменной x: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Значение функции равно: " + ds.Calculate(x));
+        }
+    }
+}
