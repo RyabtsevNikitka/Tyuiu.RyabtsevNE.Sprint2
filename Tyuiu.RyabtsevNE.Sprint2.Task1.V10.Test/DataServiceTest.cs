2590b2806d60344f45c211ca455c5db08b66c6ff
nuke_ <nuke_@N3KITOS1K>
2024-10-15 19:13:54 +0500

    Add project files.

---
+++ Tyuiu.RyabtsevNE.Sprint2.Task1.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.RyabtsevNE.Sprint2.Task1.V10.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint2.Task1.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int a = 455;
+            int b = 335;
+            int c = 14;
+            int d = 17;
+            bool[] res = new bool[6];
+            bool[] wait = new bool[6] { true, false, false, false, true, false };
+            CollectionAssert.AreEquivalent(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RyabtsevNE.Sprint2.Task1.V10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using Tyuiu.RyabtsevNE.Sprint2.Task1.V10.Lib;
+namespace Tyuiu.RyabtsevNE.Sprint2.Task1.V10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #2 | Выполнил: Рябцев Н. Е. | СМАРТб-24-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спритн #2                                                              *");
+            Console.WriteLine("* Тема: Создания итогого решения по спринту                              *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #10                                                            *");
+            Console.WriteLine("* Выполнил: Рябцев Никита Евгеньевич | СМАРТб-24-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*                                                                        *");
+            int a = 455;
+            int b = 335;
+            int c = 14;
+            int d = 17;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+        }
+    }
+}
