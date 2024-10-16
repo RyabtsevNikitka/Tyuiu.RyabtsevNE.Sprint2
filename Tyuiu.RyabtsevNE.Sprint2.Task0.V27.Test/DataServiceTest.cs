95c9a122855a8242bcb33b5a7c4848ff0da8737f
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-10-16 12:27:30 +0500

    Task0

---
+++ Tyuiu.RyabtsevNE.Sprint2.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.RyabtsevNE.Sprint2.Task0.V27.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint2.Task0.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
